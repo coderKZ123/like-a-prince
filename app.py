3d109c1609938f6a7d2df9003f4c416612a33fc4
coderKZ123 <zhukenson@gmail.com>
2026-02-22 13:34:56 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-st.set_page_config(page_title="Snowday War Game", layout="centered")
+st.set_page_config(page_title="War Game", layout="centered")
 
 
 # --- Session state ---
